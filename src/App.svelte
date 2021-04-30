c07ae75b981059291a2a54238c1d2ae3e027bc81
tnfssc <tnfssc@gmail.com>
2021-05-01 03:10:49 +0530

    Svelte-tac-toe init

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import Button from "./lib/Button.svelte";
+  import type { OnClickType, ButtonValue } from "./lib/Button.svelte";
+
+  let ticTacToeState: ButtonValue[][] = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+
+  const handleClick: OnClickType = (_, position) => {
+    if (!position) return;
+    ticTacToeState[position.y][position.x] = "X";
+  };
+</script>
+
+<main>
+  {#each ticTacToeState as row, i}
+    <div style="display: flex;">
+      {#each row as cell, j}
+        <Button onClick={handleClick} position={{ x: j, y: i }} value={cell} style="margin: 2px" />
+      {/each}
+    </div>
+  {/each}
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans",
+      "Helvetica Neue", sans-serif;
+  }
+</style>
---
+++ src/lib/Button.svelte
@@ -0,0 +1,34 @@
+<script lang="ts" context="module">
+  export type ButtonValue = "" | "X" | "O";
+  export type Position = {
+    x: number;
+    y: number;
+  };
+  export type OnClickType = (
+    e: MouseEvent & {
+      currentTarget: EventTarget & HTMLButtonElement;
+    },
+    position?: Position
+  ) => void;
+</script>
+
+<script lang="ts">
+  export let style: string = "";
+  export let value: ButtonValue = "";
+  export let position: Position;
+  export let onClick: OnClickType;
+  const handleClick: svelte.JSX.MouseEventHandler<HTMLButtonElement> = e => onClick(e, position);
+</script>
+
+<button on:click={handleClick} {style}>
+  {value}
+</button>
+
+<style>
+  button {
+    min-width: 48px;
+    min-height: 48px;
+    background-color: white;
+    border-color: black;
+  }
+</style>
