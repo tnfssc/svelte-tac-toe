bc8e23bc1f925cb6d479f3dbaa75653910fa3f2b
tnfssc <tnfssc@gmail.com>
2021-05-01 16:46:57 +0530

    Refactored code

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,9 @@
 <script lang="ts">
-  import Button from "./lib/Button.svelte";
-  import type { OnClickType } from "./types/TicTacToe";
-  import { createTicTacToeStore, createTurnStore, endGame } from "./store/TicTacToeStore";
-
-  let ticTacToeState = createTicTacToeStore(3);
-  let currentTurn = createTurnStore();
-  const handleClick: OnClickType = (_, position) => {
-    if (!position || winner !== "") return;
-    if ($ticTacToeState[position.y][position.x] !== "") return;
-    ticTacToeState.update(position, $currentTurn);
-    currentTurn.toggle();
-  };
-  let winner = "";
-  $: winner = endGame($ticTacToeState);
+  import IndexPage from "./pages/index.svelte";
 </script>
 
 <main>
-  {#each $ticTacToeState as row, i}
-    <div style="display: flex;">
-      {#each row as cell, j}
-        <Button onClick={handleClick} position={{ x: j, y: i }} value={cell} style="margin: 2px" />
-      {/each}
-    </div>
-  {/each}
-  <!-- {$ticTacToeState} -->
-  Winner: {winner}
+  <IndexPage />
 </main>
 
 <style>
